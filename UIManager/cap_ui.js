5d11ce2e3730388e5ad342dc64bba73af9735df1
Michael Schilonka <michael@schilonka.de>
2012-09-19 17:33:07 +0200

    added UIManager class

--- UIManager/cap_ui.js
+++ UIManager/cap_ui.js
@@ -1,7 +1,7 @@
 /**
  * Returns the version of this UIManager.
  */
-function getUIVersion(){
+function getUIVersion() {
 	return "v0.1";
 }
 
@@ -9,143 +9,149 @@ ComponentIDs = new Hash();
 ComponentAffiliation = new Hash();
 ComponentResolver = new Hash();
 
+var UIManager = new Class({
 
-function buildInitialLayout(){
-	var config = getInitialConfigFile();
-	registerComponentIDs(config.register);
-	
-	affiliateComponents(config.affiliation);
-	
-	buildContainer(config.layout,$("ground"));
-	
-	ComponentResolver.forEach(function(comp,name){
-		comp.build(name);
-	});
-};
+	buildInitialLayout : function() {
+		var config = getInitialConfigFile();
+		this.registerComponentIDs(config.register);
 
-function registerComponentIDs(config){
-	for(var key in config){
-		if(config[key] instanceof Array){
-			config[key].each(function(name){
-				ComponentIDs.set(name,key);
-			});
-		}
-		else{
-			ComponentIDs.set(config[key],key);
-		}
-	}
-};
+		this.affiliateComponents(config.affiliation);
 
-function triggerAffiliatedComponents(sender){
-	if(sender instanceof EventInformation){
-		var slave = ComponentAffiliation.get(sender.sender);
-		if(slave != undefined){
-			if(slave instanceof Array){
-				slave.each(function(comp){
-					ComponentResolver.get(comp).getInstance().test();
+		this.buildContainer(config.layout, $("ground"));
+
+		ComponentResolver.forEach(function(comp, name) {
+			comp.build(name);
+		});
+	},
+
+	registerComponentIDs : function(config) {
+		for ( var key in config) {
+			if (config[key] instanceof Array) {
+				config[key].each(function(name) {
+					ComponentIDs.set(name, key);
 				});
+			} else {
+				ComponentIDs.set(config[key], key);
 			}
-			else{
-				ComponentResolver.get(slave).getInstance().test();
+		}
+	},
+	triggerAffiliatedComponents : function(sender) {
+		if (sender instanceof EventInformation) {
+			var slave = ComponentAffiliation.get(sender.sender);
+			if (slave != undefined) {
+				if (slave instanceof Array) {
+					slave.each(function(comp) {
+						ComponentResolver.get(comp).getInstance().test();
+					});
+				} else {
+					ComponentResolver.get(slave).getInstance().test();
+				}
 			}
 		}
-	}
-}
+	},
 
-function affiliateComponents(config){
-	for(var key in config){
-		ComponentAffiliation.set(key, config[key]);
-	}
-};
+	affiliateComponents : function(config) {
+		for ( var key in config) {
+			ComponentAffiliation.set(key, config[key]);
+		}
+	},
 
-function buildContainer(myPlacement, myContainer){
-	if(myPlacement.header != undefined){
-		if(myPlacement.header == "[object Object]"){
-			var div = new Element("div",{
-				id : myContainer.id + ".header",
-			});
-			$(myContainer).adopt(div);
-			buildContainer(myPlacement.header, div);
-		} else{
-		
-		var div = new Element("div", {
-			id : myContainer.id + ".header",
-		});
-		var comp = componentManager.initializeComponent(ComponentIDs.get(myPlacement.header), div);
-		ComponentResolver.set(myPlacement.header, comp);
-		$(myContainer).adopt(div);
+	buildContainer : function(myPlacement, myContainer) {
+		if (myPlacement.header != undefined) {
+			if (myPlacement.header == "[object Object]") {
+				var div = new Element("div", {
+					id : myContainer.id + ".header",
+				});
+				$(myContainer).adopt(div);
+				buildContainer(myPlacement.header, div);
+			} else {
+
+				var div = new Element("div", {
+					id : myContainer.id + ".header",
+				});
+				var comp = componentManager.initializeComponent(ComponentIDs
+						.get(myPlacement.header), div);
+				ComponentResolver.set(myPlacement.header, comp);
+				$(myContainer).adopt(div);
+			}
 		}
-	}
-	if(myPlacement.footer != undefined){
-		if(myPlacement.footer == "[object Object]"){
-			var div = new Element("div",{
-				id : myContainer.id + ".footer",
-			});
-			$(myContainer).adopt(div);
-			buildContainer(myPlacement.footer, div);
-		} else{
-		var div = new Element("div",{
-			id : myContainer.id + ".footer",
-		});
-		var comp = componentManager.initializeComponent(ComponentIDs.get(myPlacement.footer), div);
-		ComponentResolver.set(myPlacement.footer, comp);
-		$(myContainer).adopt(div);
+		if (myPlacement.footer != undefined) {
+			if (myPlacement.footer == "[object Object]") {
+				var div = new Element("div", {
+					id : myContainer.id + ".footer",
+				});
+				$(myContainer).adopt(div);
+				buildContainer(myPlacement.footer, div);
+			} else {
+				var div = new Element("div", {
+					id : myContainer.id + ".footer",
+				});
+				var comp = componentManager.initializeComponent(ComponentIDs
+						.get(myPlacement.footer), div);
+				ComponentResolver.set(myPlacement.footer, comp);
+				$(myContainer).adopt(div);
+			}
 		}
-	}
-	if(myPlacement.left != undefined){
-		if(myPlacement.left == "[object Object]"){
-			var div = new Element("div",{
-				id : myContainer.id + ".left",
-			});
-			$(myContainer).adopt(div);
-			buildContainer(myPlacement.left, div);
-		} else{
-		var div = new Element("div",{
-			id : myContainer.id + ".left",
-		});
-		var comp = componentManager.initializeComponent(ComponentIDs.get(myPlacement.left), div);
-		ComponentResolver.set(myPlacement.left, comp);
-		$(myContainer).adopt(div);
+		if (myPlacement.left != undefined) {
+			if (myPlacement.left == "[object Object]") {
+				var div = new Element("div", {
+					id : myContainer.id + ".left",
+				});
+				$(myContainer).adopt(div);
+				buildContainer(myPlacement.left, div);
+			} else {
+				var div = new Element("div", {
+					id : myContainer.id + ".left",
+				});
+				var comp = componentManager.initializeComponent(ComponentIDs
+						.get(myPlacement.left), div);
+				ComponentResolver.set(myPlacement.left, comp);
+				$(myContainer).adopt(div);
+			}
 		}
-	}
-	if(myPlacement.right != undefined){
-		if(myPlacement.right == "[object Object]"){
-			var div = new Element("div",{
-				id : myContainer.id + ".right",
-			});
-			$(myContainer).adopt(div);
-			buildContainer(myPlacement.right, div);
-		} else{
-		var div = new Element("div",{
-			id : myContainer.id + ".right",
-		});
-		var comp = componentManager.initializeComponent(ComponentIDs.get(myPlacement.right), div);
-		ComponentResolver.set(myPlacement.right, comp);
-		$(myContainer).adopt(div);
+		if (myPlacement.right != undefined) {
+			if (myPlacement.right == "[object Object]") {
+				var div = new Element("div", {
+					id : myContainer.id + ".right",
+				});
+				$(myContainer).adopt(div);
+				buildContainer(myPlacement.right, div);
+			} else {
+				var div = new Element("div", {
+					id : myContainer.id + ".right",
+				});
+				var comp = componentManager.initializeComponent(ComponentIDs
+						.get(myPlacement.right), div);
+				ComponentResolver.set(myPlacement.right, comp);
+				$(myContainer).adopt(div);
+			}
 		}
-	}
-	if(myPlacement.center != undefined){
-		if(myPlacement.left == "[object Object]"){
-			var div = new Element("div",{
-				id : myContainer.id + ".center",
-			});
-			$(myContainer).adopt(div);
-			buildContainer(myPlacement.center, div);
-		} else{
-		var div = new Element("div",{
-			id : myContainer.id + ".center ",
-		});
-		var comp = componentManager.initializeComponent(ComponentIDs.get(myPlacement.center), div);
-		ComponentResolver.set(myPlacement.center, comp);
-		$(myContainer).adopt(div);
+		if (myPlacement.center != undefined) {
+			if (myPlacement.left == "[object Object]") {
+				var div = new Element("div", {
+					id : myContainer.id + ".center",
+				});
+				$(myContainer).adopt(div);
+				buildContainer(myPlacement.center, div);
+			} else {
+				var div = new Element("div", {
+					id : myContainer.id + ".center ",
+				});
+				var comp = componentManager.initializeComponent(ComponentIDs
+						.get(myPlacement.center), div);
+				ComponentResolver.set(myPlacement.center, comp);
+				$(myContainer).adopt(div);
+			}
 		}
+
 	}
-};
+
+});
 
 var EventInformation = new Class({
-    initialize: function(mySender, myActionName, myActionType){
-    	this.sender = mySender;
-        this.actionName = myActionName;
-        this.actionType = myActionType;
-    },
+	initialize : function(mySender, myActionName, myActionType) {
+		this.sender = mySender;
+		this.actionName = myActionName;
+		this.actionType = myActionType;
+	},
 });
