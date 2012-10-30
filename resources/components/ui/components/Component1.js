11ff04066ac278aacfaca5ba24aa1e1b730c511a
Michael Schilonka <michael@schilonka.de>
2012-10-30 12:24:44 +0100

    removed debris

--- resources/components/ui/components/Component1.js
+++
@@ -1,9 +0,0 @@
-var Component1 = new Class({
-    initialize: function(name){
-        this.name = name;
-    },
-	isInjected : function(){
-		alert("Successful injected Component1!");
-}
-    
-});
--- resources/components/ui/components/timeline.js
+++
@@ -1,2 +0,0 @@
-//Some timeline, an yearly, monthly or even weekly overview of the recent posts
-//The idea is to create a structure from folder directories
